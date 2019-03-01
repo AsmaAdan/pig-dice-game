0312ceed803299ba8fb4721d108955e6b0503c41
AsmaAdan <asmaahmed.adan@gmail.com>
2019-03-01 17:51:03 +0300

    remove the double dice and make it single dice

--- js/scripts.js
+++ js/scripts.js
@@ -5,25 +5,24 @@ $("#newGame").click(function(event){
 
 var score= [0,0];
 var gamePlay=true;
-//var setInterval(function () {
+//var setInterva(function () {
 
-//}, 1000);
-
+//
 
 if (totalScore === 100)
    alert("The game has been won!");
 
 function roll (){
 var a= document.getElementById("dice1");
-var b= document.getElementById("dice2");
+//var b= document.getElementById("dice2");
 var c= Math.floor(Math.random()*6)+1;
-var d= Math.floor(Math.random()*6)+1;
+//var d= Math.floor(Math.random()*6)+1;
 dice1.innerHTML = c;
-dice2.innerHTML = d;
-current.innerHTML=c+d;
+//dice2.innerHTML = d;
+current.innerHTML=c
 }
 
-//function player(currentScore, initialScore){
-//this.currentScore =
-//this.initialScore=
+//function scores (currentScore, initialScore) {
+  //this.currentScore =
+
 //}
