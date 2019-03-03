6282d172fc78dd4999c3d5fa84770e3549bee80f
AsmaAdan <asmaahmed.adan@gmail.com>
2019-03-04 00:04:27 +0300

    change function, add a prototype

--- js/scripts.js
+++ js/scripts.js
@@ -3,26 +3,37 @@ $("#newGame").click(function(event){
         $("#slide").show();
 });
 
-var score= [0,0];
 var gamePlay=true;
-//var setInterva(function () {
-
-//
-
-if (totalScore === 100)
-   alert("The game has been won!");
+//var totalScore=[now + current];
+var score= [0];
+//var now=[0];
+var a=[1,2,3,4,5,6]
 
 function roll (){
-var a= document.getElementById("dice1");
-//var b= document.getElementById("dice2");
-var c= Math.floor(Math.random()*6)+1;
-//var d= Math.floor(Math.random()*6)+1;
+var b = document.getElementById("dice1");
+var c= a[Math.floor(Math.random()*6)];
+var d= score.unshift(c);
+//var e= now.unshift(c);
 dice1.innerHTML = c;
-//dice2.innerHTML = d;
-current.innerHTML=c
+current.innerHTML=score.unshift(a);
+total.innerHTML= c + d;
+
+
+if (c === 1) {
+    alert("Sorry, you rolled a one!");
+    current.innerHTML=0;
+    total.innerHTML= 0;
+    function clear(){
+      score.length = 0;
+    }
+    next.innerHTML=score.unshift(a);
+    result.innerHTML= c + d;
+
+  }
 }
-
-//function scores (currentScore, initialScore) {
-  //this.currentScore =
-
-//}
+if (total === 100  || current ===100) {
+    alert("you are a winner");
+  }
+  function hold(){
+    return total + current
+  }
