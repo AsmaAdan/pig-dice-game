d71222519bc064c126d4fae7d35d3b60f8a29eef
AsmaAdan <47319705+AsmaAdan@users.noreply.github.com>
2019-03-04 16:06:03 +0300

    fix spacing and indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,54 @@
-$("#newGame").click(function(event){
-        event.preventDefault();
-        $("#slide").show();
+$("#newGame").click(function(event) {
+	event.preventDefault();
+	$("#slide").show();
 });
+var gamePlay = true;
+var totalScore = [now + current];
+var score = [0];
+var now = [0];
+var a = [1, 2, 3, 4, 5, 6]
 
-var gamePlay=true;
-var totalScore=[now + current];
-var score= [0];
-var now=[0];
-var a=[1,2,3,4,5,6]
-
-function roll (){
-var b = document.getElementById("dice1");
-var c= a[Math.floor(Math.random()*6)];
-var d= score.unshift(c);
-var e= now.unshift(c);
-dice1.innerHTML = c;
-current.innerHTML=score.unshift(a);
-total.innerHTML= c + d;
-
-
-if (c === 1) {
-    alert("Sorry, you rolled a one!");
-    current.innerHTML=0;
-    total.innerHTML= 0;
-    score=[0];
-    next.innerHTML=score.unshift(a);
-    result.innerHTML= c + d;
-    $("#roll").hide();
-    $("#roll2").show();
-
-  }
+function roll() {
+	var b = document.getElementById("dice1");
+	var c = a[Math.floor(Math.random() * 6)];
+	var d = score.unshift(c);
+	var e = now.unshift(c);
+	dice1.innerHTML = c;
+	current.innerHTML = score.unshift(a);
+	total.innerHTML = c + d;
+	if (c === 1) {
+		alert("Sorry, you rolled a one!");
+		current.innerHTML = 0;
+		total.innerHTML = 0;
+		score = [0];
+		next.innerHTML = score.unshift(a);
+		result.innerHTML = c + d;
+		$("#roll").hide();
+		$("#roll2").show();
+	}
 }
-  function roll2 (){
-  var b = document.getElementById("dice1");
-  var c= a[Math.floor(Math.random()*6)];
-  var d= score.unshift(c);
-  var e= now.unshift(c);
-  dice1.innerHTML = c;
-  next.innerHTML=score.unshift(a);
-  result.innerHTML= c + d;
 
-  if (c === 1) {
-      alert("Sorry, you rolled a one!");
-      current.innerHTML=0;
-      total.innerHTML= 0;
-      score=[0];
-      current.innerHTML=score.unshift(a);
-      total.innerHTML= c + d;
-      $("#roll2").hide();
-      $("#roll").show();
+function roll2() {
+	var b = document.getElementById("dice1");
+	var c = a[Math.floor(Math.random() * 6)];
+	var d = score.unshift(c);
+	var e = now.unshift(c);
+	dice1.innerHTML = c;
+	next.innerHTML = score.unshift(a);
+	result.innerHTML = c + d;
+	if (c === 1) {
+		alert("Sorry, you rolled a one!");
+		current.innerHTML = 0;
+		total.innerHTML = 0;
+		score = [0];
+		current.innerHTML = score.unshift(a);
+		total.innerHTML = c + d;
+		$("#roll2").hide();
+		$("#roll").show();
+	}
 }
+if (score === 100) {
+	alert("you are a winner");
+	rollBtn.attr("disabled", "disabled");
 }
-if (score=== 100) {
-    alert("you are a winner");
-     rollBtn.attr("disabled", "disabled");
-  }
-  function hold(){
-    return total + current
-  }
+
