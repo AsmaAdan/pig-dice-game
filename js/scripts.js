423d4185ccad88ee9fc934ec973873bfbb69be11
AsmaAdan <asmaahmed.adan@gmail.com>
2019-03-04 08:01:48 +0300

    make score 0 to clear everything

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,16 @@ $("#newGame").click(function(event){
 });
 
 var gamePlay=true;
-//var totalScore=[now + current];
+var totalScore=[now + current];
 var score= [0];
-//var now=[0];
+var now=[0];
 var a=[1,2,3,4,5,6]
 
 function roll (){
 var b = document.getElementById("dice1");
 var c= a[Math.floor(Math.random()*6)];
 var d= score.unshift(c);
-//var e= now.unshift(c);
+var e= now.unshift(c);
 dice1.innerHTML = c;
 current.innerHTML=score.unshift(a);
 total.innerHTML= c + d;
@@ -23,14 +23,12 @@ if (c === 1) {
     alert("Sorry, you rolled a one!");
     current.innerHTML=0;
     total.innerHTML= 0;
-    function clear(){
-      score.length = 0;
-    }
+    score=[0];
     next.innerHTML=score.unshift(a);
     result.innerHTML= c + d;
 
   }
-if (c+d === 100) {
+if (score=== 100) {
     alert("you are a winner");
      rollBtn.attr("disabled", "disabled");
   }
